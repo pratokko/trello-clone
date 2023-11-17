9196d893734662298d0d76be5ca8de10f9435755
pratokko <pratokko3@gmail.com>
2023-11-17 22:08:23 +0300

    the side navigation

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { OrgControl } from "./_components/org-control";
+
+const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
+  return <>
+  <OrgControl />
+  {children}
+  </>;
+};
+export default OrganizationIdLayout;
